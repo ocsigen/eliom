a2dcc942a58cbc16f3b8d7b0ae0b4a97f0c92efd
balat.at.pps.jussieu.fr <none>
2005-09-14 15:55:57 +0200

    0.0.3
    Version avec des RegisterPage RegisterIntPage RegisterStringPage et idem pour botes, et forget_param_box etc.
    
    darcs-hash:20050914135557-e63a0-8a367d729614d85fda44d518cfb661ac9bf4f5bb

--- krokoutils/krokopages.ml
+++ krokoutils/krokopages.ml
@@ -10,7 +10,7 @@ class virtual ['a] generic_item = object
 
 end
 
-class virtual box boxparam sessionparam pageparam = object
+class virtual box = object
   inherit [Xhtmlpp.xhtmlcont] generic_item
 
   (** print the page in xhtml using xml extended syntax xmlp4 *)
@@ -18,26 +18,32 @@ class virtual box boxparam sessionparam pageparam = object
 end
 
 (** A box that prints an error message *)
-class empty_box () sessionparam pageparam = object
-  inherit box () sessionparam pageparam
+class empty_box = object
+  inherit box
   method print = << <b>empty box</b> >>
 end
 
 module RegisterBox = MakeRegister(struct 
-				    type t = unit -> unit -> box
-				    let default_content = new empty_box ()
+				    type t = box
+				    let default_content = new empty_box
 				  end)
 module RegisterIntBox = MakeRegister(struct 
-				       type t = unit -> int -> box
-				       let default_content = new empty_box ()
+				       type t = int -> box
+				       let default_content = 
+					 (fun i -> new empty_box)
 				     end)
+module RegisterStringBox = MakeRegister(struct 
+					  type t = string -> box
+					  let default_content = 
+					    (fun s -> new empty_box)
+					end)
 
   
 (*****************************************************************************)
 (** Some usefull boxes: *)
 (** Title *)
-class title_box titre sessionparam pageparam = object
-  inherit box titre sessionparam pageparam
+class title_box titre = object
+  inherit box
   method print = << <h1>$str:titre$</h1> >>
 end
 
@@ -46,26 +52,42 @@ let fold_title_box =
 
 
 (** A simple box that prints something *)
-class text_box msg sessionparam pageparam = object
-  inherit box msg sessionparam pageparam
+class text_box msg = object
+  inherit box
   method print = << <div>$str:msg$</div> >>
 end
 
 let fold_text_box = 
-  RegisterBox.register ~name:"text_box" 
-    ~constructor:(new text_box)
+  RegisterBox.register ~name:"text_box" ~constructor:(new text_box)
 
 
 (** A simple box that prints a message of the db *)
-class string_message_box () sessionparam key = object
-  inherit box () sessionparam key
+class string_message_box key = object
+  inherit box
   val msg = StringMessage.dbget key
   method print = << <div>$str:msg$</div> >>
 end
 
 let fold_string_message_box = 
   RegisterIntBox.register ~name:"string_message_box" 
-    ~constructor:(new string_message_box)
+    ~constructor:(fun () -> new string_message_box)
+
+(** A box that contains another box but removing a parameter *)
+class forget_param_box boxdescr param = object
+  inherit box
+  val b = (RegisterBox.unfold boxdescr)#print
+  method print = b
+end
+
+let fold_forget_param_int_box = 
+  RegisterIntBox.register ~name:"forget_param_int_box" 
+    ~constructor:(new forget_param_box)
+
+let fold_forget_param_string_box = 
+  RegisterStringBox.register ~name:"forget_param_string_box"
+    ~constructor:(new forget_param_box)
+
+
 
 
 
@@ -79,7 +101,7 @@ let fold_string_message_box =
     We need two constructors, one to create pages from database,
     the other one manually.
  *)
-class page_ sessionparam pageparam = object (moi)
+class page_ = object (moi)
 
   inherit [Xhtmlpp.xhtml] generic_item
 
@@ -91,19 +113,19 @@ class page_ sessionparam pageparam = object (moi)
 
 end
 
-class page bl sessionparam pageparam = object
+class page bl = object
 
-  inherit page_ sessionparam pageparam
+  inherit page_
   method boxlist = bl
 
 end
 
-class page_fromdb boxdescrlist sessionparam pageparam = object
+class page_fromdb boxdescrlist = object
 
-  inherit page_ sessionparam pageparam
+  inherit page_
 
   val bl = 
-    List.map (fun a -> (RegisterBox.unfold a) sessionparam pageparam)
+    List.map (fun a -> (RegisterBox.unfold a))
     boxdescrlist
 
   method boxlist = bl
@@ -112,13 +134,51 @@ end
 
 module RegisterPage = 
   MakeRegister(struct 
-		 type t = unit -> unit -> page_
-		 let default_content = new page [new empty_box () () ()]
+		 type t = page_
+		 let default_content = 
+		   new page [new empty_box]
 	       end)
 
 
 let fold_page_fromdb = 
   RegisterPage.register ~name:"page_fromdb" 
     ~constructor:(new page_fromdb)
+
+
+(* Pages that take an int paramater *)
+class param_page_fromdb boxdescrlist param = object
+
+  inherit page_
+
+  val bl = 
+    List.map (fun a -> (RegisterIntBox.unfold a) param)
+      boxdescrlist
+
+  method boxlist = bl
+
+end
+
+module RegisterIntPage = 
+  MakeRegister(struct 
+		 type t = int -> page_
+		 let default_content = 
+		   fun _ -> new page [new empty_box]
+	       end)
+
+let fold_int_page_fromdb = 
+  RegisterIntPage.register ~name:"int_page_fromdb" 
+    ~constructor:(new param_page_fromdb)
+
+
+module RegisterStringPage = 
+  MakeRegister(struct 
+		 type t = string -> page_
+		 let default_content = 
+		   fun _ -> new page [new empty_box]
+	       end)
+
+let fold_string_page_fromdb = 
+  RegisterIntPage.register ~name:"string_page_fromdb" 
+    ~constructor:(new param_page_fromdb)
 
 
--- krokoutils/krokoxample.ml
+++ krokoutils/krokoxample.ml
@@ -5,21 +5,14 @@ open Krokodata
 open Krokopages
 
 
-
-
 (** Some boxes and a page we need for the example: *)
 (** A page displaying a message *)
-let page_message = register_new_url (Url ["msg"]) (_int "num")
-  (fun k -> 
-     (new page
-	[new title_box "Voici le message" () (); 
-	 new string_message_box () () k] () k)#print
-  )
+let page_message = new_url (Url ["msg"]) (_int "num")
 
 (** A box that prints the beginning of a message, with a link to the 
     full message *)
-class string_message_header_box () sessionparam key = object
-  inherit box () sessionparam key
+class string_message_header_box key = object
+  inherit box
   val msg = StringMessage.dbget key
   val l = link "see" page_message key
   method print =  << <div> $str:msg$ $l$ </div> >>
@@ -27,14 +20,14 @@ end
 
 let fold_string_message_header_box = 
   RegisterIntBox.register ~name:"string_message_header_box" 
-    ~constructor:(new string_message_header_box)
+    ~constructor:(fun () -> new string_message_header_box)
 
 (** A box that prints a list of a message headers *)
-class string_messages_headers_list_box key sessionparam pageparam = object
-  inherit box key sessionparam pageparam
+class string_messages_headers_list_box key = object
+  inherit box
   val msglist = 
     List.map 
-      (fun n -> (new string_message_header_box () sessionparam n)#print)
+      (fun n -> (new string_message_header_box n)#print)
       (MessagesList.dbget key)
   method print = << <div>$list:msglist$</div> >>
 end
@@ -60,7 +53,7 @@ let messageslist_number =
 	  StringMessage.dbinsert "Ceci est un septième message"])
 
 
-let example_page_number = 
+let example_main_page_number = 
   Krokopersist.make_persistant_lazy "example_page_number"
     (fun () -> 
        dbinsertdyn
@@ -69,91 +62,23 @@ let example_page_number =
 	     fold_string_messages_headers_list_box 
 	       (Krokopersist.get messageslist_number)]))
 
+let example_msg_page_number = 
+  Krokopersist.make_persistant_lazy "example_msg_page_number"
+    (fun () -> 
+       dbinsertdyn
+	 (fold_int_page_fromdb
+	    [fold_forget_param_int_box (fold_title_box "Voici le message"); 
+	     fold_string_message_box ()])
+    )
+
 (* -- End population of the database with an example *)
 
 
+let _ = register_url page_message
+  (fun n -> 
+     ((RegisterIntPage.dbget 
+	(Krokopersist.get example_msg_page_number)) n)#print)
 
-let mapage = (RegisterPage.dbget (Krokopersist.get example_page_number)) () ()
+let objsav = register_new_url (Url []) _noparam 
+  (RegisterPage.dbget (Krokopersist.get example_main_page_number))#print
 
-let objsav = register_new_url (Url []) _noparam (mapage#print)
-
-
-
-
-
-
-
-
-(* Other tries:
-
-(* With objects, without savable pages: *)
-let mapage2 = 
-  new page
-    [new_title_box "Kikoo !"; 
-     new_string_messages_list_box (Krokopersist.get messageslist_number)]
-
-let nosav = register_new_url (Url ["nosav"]) _noparam (mapage2#print)
-
-(* Without objects and savable pages: *)
-open Krokoboites
-
-let noobj = register_new_url (Url ["noobj"]) _noparam
-  (let l = 
-     [(title_box "Kiko Kroko !");
-      (string_messages_list_box (Krokopersist.get messageslist_number))]
-   in << <html> $list:l$ </html> >>)
-
-
-
-
-
-(** Main page *)
-let _ = register_new_url (Url []) _noparam
-  (let l = link "With objects and savable pages" objsav in
-   let ll = link "With objects, no savable pages" nosav in
-   let lll = link "Without object and savable pages" noobj in
-     << <html> $l$ <br/> $ll$ <br/> $lll$ <br/> </html> >>)
-
-
-
-(* dans krokoboites.ml :*)
-open Krokodata
-open Omlet
-
-(******************************************************************)
-(* The boxes that can appear in pages *)
-
-(** Title *)
-let title_box titre = << <h1>$str:titre$</h1> >>
-
-(** A box that prints an error message *)
-let error_box msg = << <b>$str:msg$</b> >>
-
-(** A simple box that prints a message of the db *)
-let string_message_box key =
-  let msg = StringMessage.dbget key
-  in << $str:msg$ >>
-
-(** A page displaying a message *)
-let page_message = register_new_url (Url ["msg"]) (_int "num")
-  (fun k -> 
-     (let l = 
-	[(title_box "Kiko Kroko !");
-	 (string_message_box k)]
-      in << <html> $list:l$ </html> >>)
-  )
-
-(** A box that prints the begining of a message, with a link to the 
-    full message *)
-let beginning_of_string_message_box key =
-  let msg = StringMessage.dbget key
-  in let l = link "see" page_message key
-  in << <div> $str:msg$ $l$ </div> >>
-
-(** A box that prints a list of messages *)
-let string_messages_list_box key =
-  let l = List.map beginning_of_string_message_box (MessagesList.dbget key)
-  in << <div> $list:l$ </div> >>
-
-
-*)
