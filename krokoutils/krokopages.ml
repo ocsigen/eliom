c5e3a1334e11ef81792282fc603645d9912fb0d9
balat.at.pps.jussieu.fr <none>
2005-09-19 14:12:28 +0200

    0.0.6
    with users and rights (first try)
    
    darcs-hash:20050919121228-e63a0-ebebd8fd628815cef60e9fce1358ee7dfb6883c2

--- krokoutils/krokopages.ml
+++ krokoutils/krokopages.ml
@@ -6,14 +6,26 @@ open Krokodata
 (** A box that prints an error message *)
 let empty_box = << <b>empty box</b> >>
 
-module RegisterBox = MakeRegister(struct 
-				    type t = Xhtmlpp.xhtmlcont
-				    let default_content = empty_box
-				  end)
-module RegisterIntBox = MakeRegister(struct 
-				       type t = int -> Xhtmlpp.xhtmlcont
-				       let default_content i = empty_box
-				     end)
+module RegisterBox = 
+  MakeRegister(struct 
+		 type t = Xhtmlpp.xhtmlcont
+		 let default_content = empty_box
+	       end)
+module RegisterUserBox = 
+  MakeRegister(struct 
+		 type t = Rights.user -> Xhtmlpp.xhtmlcont
+		 let default_content u = empty_box
+	       end)
+module RegisterIntBox = 
+  MakeRegister(struct 
+		 type t = int -> Xhtmlpp.xhtmlcont
+		 let default_content i = empty_box
+	       end)
+module RegisterUserIntBox = 
+  MakeRegister(struct 
+		 type t = Rights.user -> int -> Xhtmlpp.xhtmlcont
+		 let default_content u i = empty_box
+	       end)
 
   
 (*****************************************************************************)
@@ -33,17 +45,17 @@ let fold_text_box =
 
 
 (** A simple box that prints a message of the db *)
-let string_message_box key =
-  let msg = StringMessage.dbget key
+let string_message_box key user =
+  let msg = StringMessage.dbget user key
   in << <div>$str:msg$</div> >>
 
-let fold_string_message_box = 
-  RegisterBox.register ~name:"string_message_box" 
+let fold_string_message_userbox = 
+  RegisterUserBox.register ~name:"string_message_userbox" 
     ~constructor:string_message_box
 
-let fold_string_message_intbox = 
-  RegisterIntBox.register ~name:"string_message_intbox" 
-    ~constructor:(fun () -> string_message_box)
+let fold_string_message_userintbox = 
+  RegisterUserIntBox.register ~name:"string_message_userintbox" 
+    ~constructor:(fun () u i -> string_message_box i u)
 
 
 
@@ -70,18 +82,18 @@ module RegisterPage =
 	       end)
 
 
-let fold_page_fromdb = 
+let fold_page = 
   RegisterPage.register ~name:"page_fromdb" ~constructor:page_fromdb
 
 
 (** We create a new kind of pages, that can contain either 
     Boxes or boxes with a parameter
 *)
-let param_page_fromdb boxdescrlist n = 
+let intpage_fromdb boxdescrlist p = 
   page
     (List.map
        (function 
-	    `ParamBox a -> (RegisterIntBox.unfold a) n
+	    `ParamBox a -> (RegisterIntBox.unfold a) p
 	  | `Box a -> (RegisterBox.unfold a))
        boxdescrlist)
 
@@ -93,11 +105,80 @@ module RegisterIntPage =
 		 let default_content i = empty_page
 	       end)
 
-let fold_int_page
+let fold_intpage
     : [ `Box of Krokodata.Dyn.t | `ParamBox of Krokodata.Dyn.t ] list 
     -> Krokodata.Dyn.t =
-  (RegisterIntPage.register ~name:"int_page_fromdb" 
-     ~constructor:param_page_fromdb)
+  RegisterIntPage.register ~name:"intpage" ~constructor:intpage_fromdb
+(* Ici je suis obligé de préciser le type sinon il y a un type _'a qui
+   ne peut être généralisé. 
+   Rajouter un paramètre résoud le problème de typage mais conduit à un 
+   programme FAUX parce que l'enregistrement de la page n'est pas effectué
+   une fois pour toutes au début.
+ *)
+
+
+(** We create a new kind of pages, that can contain either 
+    Boxes or boxes with a parameter
+*)
+let userpage_fromdb boxdescrlist p = 
+  page
+    (List.map
+       (function 
+	    `ParamBox a -> (RegisterUserBox.unfold a) p
+	  | `Box a -> (RegisterBox.unfold a))
+       boxdescrlist)
+
+
+(** Pages that take one int parameter *)
+module RegisterUserPage = 
+  MakeRegister(struct 
+		 type t = Rights.user -> Xhtmlpp.xhtml
+		 let default_content u = empty_page
+	       end)
+
+let fold_userpage
+    : [ `Box of Krokodata.Dyn.t | `ParamBox of Krokodata.Dyn.t ] list 
+    -> Krokodata.Dyn.t =
+  RegisterUserPage.register ~name:"userpage_fromdb" 
+    ~constructor:userpage_fromdb
+(* Ici je suis obligé de préciser le type sinon il y a un type _'a qui
+   ne peut être généralisé. 
+   Rajouter un paramètre résoud le problème de typage mais conduit à un 
+   programme FAUX parce que l'enregistrement de la page n'est pas effectué
+   une fois pour toutes au début.
+ *)
+
+
+
+(** We create a new kind of pages, that can contain either 
+    Boxes or boxes with a page parameter or with a session parameter
+*)
+let userintpage_fromdb boxdescrlist sp p = 
+  page
+    (List.map
+       (function 
+	  | `Box a -> (RegisterBox.unfold a)
+	  | `ParamBox a -> (RegisterIntBox.unfold a) p
+	  | `SessionBox a -> (RegisterUserBox.unfold a) sp
+	  | `SessionParamBox a -> (RegisterUserIntBox.unfold a) sp p)
+       boxdescrlist)
+
+
+(** Pages that take one int parameter *)
+module RegisterUserIntPage =
+  MakeRegister(struct 
+		 type t = Rights.user -> int -> Xhtmlpp.xhtml
+		 let default_content u i = empty_page
+	       end)
+
+let fold_userintpage :
+    [ `Box of Krokodata.Dyn.t
+    | `ParamBox of Krokodata.Dyn.t
+    | `SessionBox of Krokodata.Dyn.t
+    | `SessionParamBox of Krokodata.Dyn.t ] list 
+    -> Krokodata.Dyn.t =
+  RegisterUserIntPage.register ~name:"userintpage" 
+    ~constructor:userintpage_fromdb
 (* Ici je suis obligé de préciser le type sinon il y a un type _'a qui
    ne peut être généralisé. 
    Rajouter un paramètre résoud le problème de typage mais conduit à un 
--- krokoutils/krokoxample.ml
+++ krokoutils/krokoxample.ml
@@ -1,3 +1,4 @@
+(* Copyright Vincent Balat 2005 *)
 (** An example of Kroko module using Kiko *)
 
 open Omlet
@@ -11,81 +12,69 @@ let page_message = new_url (Url ["msg"]) (_int "num")
 
 (** A box that prints the beginning of a message, with a link to the 
     full message *)
-let string_message_header_box key = 
-  let msg = StringMessage.dbget key
+let string_message_header_box key user = 
+  let msg = StringMessage.dbget user key
   in let l = link "see" page_message key
   in << <div> $str:msg$ $l$ </div> >>
 
-(* éventuellement :
-let fold_string_message_header_box = 
-  RegisterBox.register ~name:"string_message_header_box" 
-    ~constructor:string_message_header_box
-
-let fold_string_message_header_intbox = 
-  RegisterIntBox.register ~name:"string_message_header_box" 
-    ~constructor:(fun () -> string_message_header_box)
-*)
-
 (** A box that prints a list of a message headers *)
-let string_messages_headers_list_box key = 
+let string_messages_headers_list_box key user = 
   let msglist = 
     List.map 
-      (fun n -> string_message_header_box n)
-      (MessagesList.dbget key)
+      (fun n -> string_message_header_box n user)
+      (MessagesList.dbget user key)
   in << <div>$list:msglist$</div> >>
 
-let fold_string_messages_headers_list_box = 
-  RegisterBox.register ~name:"string_messages_headers_list_box" 
+let fold_string_messages_headers_list_userbox = 
+  RegisterUserBox.register ~name:"string_messages_headers_list_userbox" 
     ~constructor:string_messages_headers_list_box
-
-(* éventuellement :
-let fold_string_messages_headers_list_box = 
-  RegisterIntBox.register ~name:"string_messages_headers_list_box" 
-    ~constructor:(fun () -> string_messages_headers_list_box)
-*)
-
 
 
 (* -- Here I populate the database with some examples: *)
 
+let forumadmin = Rights.connect "forumadmin" "pwd"
+
 let messageslist_number =
   Krokopersist.make_persistant_lazy "messageslist_number"
     (fun () -> 
-       MessagesList.dbinsert
-	 [StringMessage.dbinsert "Ceci est un premier message";
-	  StringMessage.dbinsert "Ceci est un deuxième message";
-	  StringMessage.dbinsert "Ceci est un troisième message";
-	  StringMessage.dbinsert "Ceci est un quatrième message";
-	  StringMessage.dbinsert "Ceci est un cinquième message";
-	  StringMessage.dbinsert "Ceci est un sixième message";
-	  StringMessage.dbinsert "Ceci est un septième message"])
+       MessagesList.dbinsert forumadmin
+	 [StringMessage.dbinsert forumadmin "Ceci est un premier message";
+	  StringMessage.dbinsert forumadmin "Ceci est un deuxième message";
+	  StringMessage.dbinsert forumadmin "Ceci est un troisième message";
+	  StringMessage.dbinsert forumadmin "Ceci est un quatrième message";
+	  StringMessage.dbinsert forumadmin "Ceci est un cinquième message";
+	  StringMessage.dbinsert forumadmin "Ceci est un sixième message";
+	  StringMessage.dbinsert forumadmin "Ceci est un septième message"])
 
 
 let example_main_page_number = 
   Krokopersist.make_persistant_lazy "example_page_number"
     (fun () -> 
-       dbinsertdyn
-	 (fold_page_fromdb
-	    [fold_title_box "Kikoo !"; 
-	     fold_string_messages_headers_list_box 
-	       (Krokopersist.get messageslist_number)]))
+       dbinsertdyn forumadmin
+	 (fold_userpage
+	    [`Box (fold_title_box "Kikoo !"); 
+	     `ParamBox (fold_string_messages_headers_list_userbox
+			  (Krokopersist.get messageslist_number))]))
 
 let example_msg_page_number = 
   Krokopersist.make_persistant_lazy "example_msg_page_number"
     (fun () -> 
-       dbinsertdyn
-	 (fold_int_page
+       dbinsertdyn forumadmin
+	 (fold_userintpage
 	    [`Box (fold_title_box "Voici le message"); 
-	     `ParamBox (fold_string_message_intbox ())])
+	     `SessionParamBox (fold_string_message_userintbox ())])
     )
 
 (* -- End population of the database with an example *)
 
 
 let _ = register_url page_message
-  (fun n ->
-     (RegisterIntPage.dbget (Krokopersist.get example_msg_page_number)) n)
+  (fun n -> 
+     (RegisterUserIntPage.dbget Rights.anonymoususer
+	(Krokopersist.get example_msg_page_number)) Rights.anonymoususer n)
 
 let objsav = register_new_url (Url []) _noparam 
-  (RegisterPage.dbget (Krokopersist.get example_main_page_number))
+  ((RegisterUserPage.dbget
+      Rights.anonymoususer 
+      (Krokopersist.get example_main_page_number)) Rights.anonymoususer)
 
