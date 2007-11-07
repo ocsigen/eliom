2c3f494466100648a5e1d973f513c30f0879fa16
balat at pps.jussieu.fr <none>
2007-11-07 20:20:41 +0100

    small patch: adding functions in lwt_chan interface
    darcs-hash:20071107192041-1b86b-fbbc335eb40b3d327273d51539ff635aa8d94d84

--- lwt/lwt_chan.mli
+++ lwt/lwt_chan.mli
@@ -4,9 +4,13 @@ type out_channel
 val make_in_channel : (string -> int -> int -> int Lwt.t) -> in_channel
 val input_line : in_channel -> string Lwt.t
 val input_value : in_channel -> 'a Lwt.t
+val input : in_channel -> string -> int -> int -> int Lwt.t
+val really_input : in_channel -> string -> int -> int -> unit Lwt.t
+val input_char : in_channel -> char Lwt.t
 
 val make_out_channel : (string -> int -> int -> int Lwt.t) -> out_channel
 val output : out_channel -> string -> int -> int -> unit Lwt.t
 val flush : out_channel -> unit Lwt.t
 val output_string : out_channel -> string -> unit Lwt.t
 val output_value : out_channel -> 'a -> unit Lwt.t
+
