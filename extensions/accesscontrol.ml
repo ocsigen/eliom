d67c3ad80af4a404216ac8df85b0e5b49ca7e968
steph <steph@glondu.net>
2007-12-22 21:17:16 +0100

    Accesscontrol: type filter is not necessary
    We also allow subelements in <ip>, <header>... filters, remove control
    filters <allow>, <deny>, <forbidden>, <notfound>. They will somehow
    reappear later.
    
    darcs-hash:20071222201716-b6f55-f3c4f7aba80271df567f624d7219a222ba6d6a42

--- extensions/accesscontrol.ml
+++ extensions/accesscontrol.ml
@@ -38,18 +38,6 @@ open Http_frame
 
 type password = [ `Plain of string ]
 
-type filter =
-  | Filter_Ip of (int32 * int32)
-  | Filter_Path of Netstring_pcre.regexp
-  | Filter_Method of Http_frame.Http_header.http_method
-  | Filter_Header of string * Netstring_pcre.regexp
-  | Filter_Protocol of Http_frame.Http_header.proto
-  | Filter_Auth of (string -> password -> bool)
-  | Filter_Disj of filter list
-  | Filter_Conj of filter list
-  | Filter_Neg of filter
-  | Filter_Error of exn
-
 
 (*****************************************************************************)
 let rec parse_global_config = function
@@ -96,7 +84,6 @@ let _ = register_authentication_method
      | _ -> raise (Bad_config_tag_for_extension "not for htpasswd"))
 
 (*****************************************************************************)
-(* Finding access pattern *)
 
 let find_access ri =
   let rec one_access = function
@@ -110,16 +97,16 @@ let find_access ri =
     | Filter_Path regexp ->
         let r =
           Netstring_pcre.string_match
-            regexp ri.ri_sub_path_string 0 <> None
+            regexp (Lazy.force ri.ri_sub_path_string) 0 <> None
         in
         if r then
           Messages.debug
             (fun () -> "--Access control: Path "^
-              ri.ri_sub_path_string^" matches regexp")
+              (Lazy.force ri.ri_sub_path_string)^" matches regexp")
         else
           Messages.debug
             (fun () -> "--Access control: Path "^
-              ri.ri_sub_path_string^" does not match regexp");
+              (Lazy.force ri.ri_sub_path_string)^" does not match regexp");
         r
     | Filter_Method meth ->
         let r = meth = ri.ri_method in
@@ -171,7 +158,7 @@ let gen test charset = function
         end
         else begin
           Messages.debug2 "--Access control: => Access denied!";
-          Lwt.return (Ext_stop_site (Http_frame.Cookies.empty, 403))
+          Lwt.return (Ext_stop_site (ri, Http_frame.Cookies.empty, 403))
         end
       with
         | e ->
@@ -193,60 +180,115 @@ let gen test charset = function
 
  *)
 
+let parse_config path charset _ parse_fun = function
 
-let parse_config path charset _ parse_fun =
-  let parse_filter = function
-    | ("ip", ["value", s], []) ->
-        (try
-          Filter_Ip (Ocsimisc.parse_ip_netmask s)
-        with Failure _ ->
-          raise (Error_in_config_file "Bad ip/netmask value in ip filter"))
-    | ("header", ["name", s; "regexp", r], []) ->
-        (try
-          Filter_Header (s, Netstring_pcre.regexp r)
-        with Failure _ ->
-          raise (Error_in_config_file
-                   "Bad regular expression in header filter"))
-    | ("method", ["value", s], []) ->
-        (try
-          Filter_Method (Framepp.method_of_string s)
-        with Failure _ ->
-          raise (Error_in_config_file "Bad method value in method filter"))
-    | ("protocol", ["value", s], []) ->
-        (try
-          Filter_Protocol (Framepp.proto_of_string s)
-        with Failure _ ->
-          raise (Error_in_config_file "Bad protocol value in protocol filter"))
-    | ("path", ["regexp", s], []) ->
-        (try
-          Filter_Path (Netstring_pcre.regexp s)
-        with Failure _ ->
-          raise (Error_in_config_file
-                   "Bad regular expression in <path/>"))
-    | ("auth", ["authtype", "basic"; "realm", r], [sub]) ->
-        (try
-           Filter_Auth (get_authentication_method sub)
-         with Bad_config_tag_for_extension _ ->
-           raise (Error_in_config_file "Unable to find proper authentication method"))
-    | (t, _, _) -> raise (Error_in_config_file ("(accesscontrol extension) Problem with "^t^" filter in configuration file."))
-  in
-  let rec parse_sub = function
-    | Element ("allow", ["type", "and"], sub) -> Filter_Conj (List.map parse_sub sub)
-    | Element ("allow", ["type", "or"], sub) -> Filter_Disj (List.map parse_sub sub)
-    | Element ("allow", ("type", t)::attrs, sub) -> parse_filter (t, attrs, sub)
-    | Element ("deny", attrs, sub) -> Filter_Neg (parse_sub (Element ("allow", attrs, sub)))
-    | Element (t, _, _) ->
-        raise (Error_in_config_file ("(accesscontrol extension) Problem with tag <"^t^"> in configuration file."))
-    | _ -> raise (Error_in_config_file "(accesscontrol extension) Bad data")
-  in
-  function
-    | Element (("allow" | "deny"), attrs, sub) as e -> gen (parse_sub e) charset
-    | Element ("forbidden", [], []) -> 
-        gen (Filter_Error
-               (Ocsigen_http_error (Http_frame.Cookies.empty, 403))) charset
-    | Element ("notfound", [], []) -> 
-        gen (Filter_Error 
-               (Ocsigen_http_error (Http_frame.Cookies.empty, 404))) charset
+    | Element ("ip", ["value", s], sub) ->
+        let (ip32, mask) =
+          try
+            Ocsimisc.parse_ip_netmask s
+          with Failure _ ->
+            raise (Error_in_config_file "Bad ip/netmask value in ip filter")
+        in
+        gen (parse_fun sub)
+          (fun ri ->
+             let r = Int32.logand (Lazy.force ri.ri_ip32) mask = ip32 in
+             if r then
+               Messages.debug2 "--Access control: IP matches mask"
+             else
+               Messages.debug2 "--Access control: IP does not match mask";
+             r)
+
+    | Element ("header", ["name", name; "regexp", r], sub) ->
+        let regexp =
+          try
+            Netstring_pcre.regexp r
+          with Failure _ ->
+            raise (Error_in_config_file
+                     "Bad regular expression in header filter")
+        in
+        gen (parse_fun sub)
+          (fun ri ->
+             let r =
+               List.exists
+                 (fun a -> Netstring_pcre.string_match regexp a 0 <> None)
+                 (Http_headers.find_all
+                    (Http_headers.name name)
+                    ri.ri_http_frame.Http_frame.header.Http_frame.Http_header.headers)
+             in
+             if r then
+               Messages.debug2 "--Access control: Header matches regexp"
+             else
+               Messages.debug2 "--Access control: Header does not match regexp";
+             r)
+
+    | Element ("method", ["value", s], sub) ->
+        let meth =
+          try
+            Framepp.method_of_string s
+          with Failure _ ->
+            raise (Error_in_config_file "Bad method value in method filter")
+        in
+        gen (parse_fun sub)
+          (fun ri ->
+             let r = meth = ri.ri_method in
+             if r then
+               Messages.debug2 "--Access control: Method matches"
+             else
+               Messages.debug2 "--Access control: Method does not match";
+             r)
+
+    | Element ("protocol", ["value", s], sub) ->
+        let pr =
+          try
+            Framepp.proto_of_string s
+          with Failure _ ->
+            raise (Error_in_config_file "Bad protocol value in protocol filter")
+        in
+        gen (parse_fun sub)
+          (fun ri ->
+             let r = pr = ri.ri_protocol in
+             if r then
+               Messages.debug2 "--Access control: Protocol matches"
+             else
+               Messages.debug2 "--Access control: Protocol does not match";
+             r)
+
+    | Element ("path", ["regexp", s], sub) ->
+        let regexp =
+          try
+            Netstring_pcre.regexp s
+          with Failure _ ->
+            raise (Error_in_config_file
+                     "Bad regular expression in <path/>")
+        in
+        gen (parse_fun sub)
+          (fun ri ->
+             let r =
+               Netstring_pcre.string_match
+                 regexp (Lazy.force ri.ri_sub_path_string) 0 <> None
+             in
+             if r then
+               Messages.debug
+                 (fun () -> "--Access control: Path "^
+                    (Lazy.force ri.ri_sub_path_string)^" matches regexp")
+             else
+               Messages.debug
+                 (fun () -> "--Access control: Path "^
+                    (Lazy.force ri.ri_sub_path_string)^" does not match regexp");
+             r)
+
+    | Element ("auth", ["authtype", "basic"; "realm", r], auth::sub) ->
+        let _ =
+          try
+            get_authentication_method auth
+          with Bad_config_tag_for_extension _ ->
+            raise (Error_in_config_file "Unable to find proper authentication method")
+        in
+        gen (parse_fun sub)
+          (fun ri ->
+             Messages.debug2 "--Access control: Authentication failed";
+             raise (Http_error.Http_exception (401, None, None)))
+
     | Element ("iffound", [], sub) ->
         let ext = parse_fun sub in
         (*VVV DANGER: parse_fun MUST be called BEFORE the function! *)
@@ -255,6 +297,7 @@ let parse_config path charset _ parse_fun =
                Lwt.return (Ext_sub_result ext)
            | Extensions.Req_not_found (err, ri) ->
                Lwt.return (Extensions.Ext_next err))
+
     | Element ("ifnotfound", [], sub) ->
         let ext = parse_fun sub in
         (function
@@ -262,6 +305,7 @@ let parse_config path charset _ parse_fun =
                Lwt.return (Extensions.Ext_found r)
            | Extensions.Req_not_found (err, ri) ->
                Lwt.return (Ext_sub_result ext))
+
     | Element ("ifnotfound", [("code", s)], sub) ->
         let ext = parse_fun sub in
         let r = Netstring_pcre.regexp s in
@@ -273,10 +317,9 @@ let parse_config path charset _ parse_fun =
                  Lwt.return (Ext_sub_result ext)
                else
                  Lwt.return (Extensions.Ext_next err))
-    | Element (("forbidden"|"notfound"|"iffound"|"ifnotfound") as t, _, _) ->
-        raise (Error_in_config_file ("(accesscontrol extension) Problem with tag <"^t^"> in configuration file."))
+
     | Element (t, _, _) -> raise (Bad_config_tag_for_extension t)
-    | _ -> raise (Error_in_config_file "(accesscontrol extension) Bad (toplevel) data")
+    | _ -> raise (Error_in_config_file "(accesscontrol extension) Bad data")
 
 
 
