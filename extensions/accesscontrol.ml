1979af9255d84087912c3b60c39065000bafd964
steph <steph@glondu.net>
2008-02-07 19:41:34 +0100

    Moving authentication to a different module, documentation
    darcs-hash:20080207184134-b6f55-53e2bc316accce454f31d8d0b0f4304eb01549ff

--- extensions/accesscontrol.ml
+++ extensions/accesscontrol.ml
@@ -44,45 +44,6 @@ let rec parse_global_config = function
 let _ = parse_global_config (Extensions.get_config ())
 
 
-
-(*****************************************************************************)
-(* Management of basic authentication methods *)
-
-exception Bad_config_tag_for_auth of string
-
-let register_basic_authentication_method,
-  get_basic_authentication_method =
-
-  let fun_auth = ref
-    (fun config ->
-       raise (Bad_config_tag_for_auth "<unknown basic authentication method>"))
-  in
-
-  (********* register_basic_authentication_method *********)
-  (fun new_fun_auth ->
-     let old_fun_auth = !fun_auth in
-     fun_auth :=
-       (fun config ->
-          try
-            old_fun_auth config
-          with
-            | Bad_config_tag_for_auth c -> new_fun_auth config)),
-
-  (********* get_basic_authentication_method *********)
-  (fun config ->
-     !fun_auth config)
-
-
-(*****************************************************************************)
-(* Basic authentication with a predefined login/password (example) *)
-
-let _ = register_basic_authentication_method
-  (function
-     | Element ("plain", ["login", login; "password", password], _) ->
-	 (fun l p -> Lwt.return (login = l && password = p))
-     | _ -> raise (Bad_config_tag_for_extension "not for htpasswd"))
-
-
 (*****************************************************************************)
 
 exception Bad_config_tag_for_filter
@@ -290,65 +251,6 @@ let parse_config path charset _ parse_fun = function
                  Extensions.Ext_stop_site (Http_frame.Cookies.empty, 403)
                end)
       end
-
-    | Element ("auth", ["authtype", "basic"; "realm", realm], auth::sub) ->
-        (* http://www.ietf.org/rfc/rfc2617.txt *)
-        (* TODO: check that realm is correct *)
-        let auth =
-          try
-            get_basic_authentication_method auth
-          with Bad_config_tag_for_extension _ ->
-            raise (Error_in_config_file "Unable to find proper authentication method")
-        in
-        (fun rs ->
-           match rs with
-             | Extensions.Req_not_found (err, ri) ->
-                 let reject () =
-                   let h = Http_headers.add
-                     (Http_headers.name "WWW-Authenticate")
-                     (sprintf "Basic realm=\"%s\"" realm)
-                     Http_headers.empty
-                   in
-                   Messages.debug2 "--Access control (auth): invalid credentials!";
-                   fail (Http_error.Http_exception (401, None, Some h))
-                 in
-                 begin try
-                   let (login, password) =
-                     let credentials =
-                       Http_headers.find
-                         (Http_headers.name "Authorization")
-                         ri.ri_http_frame.Http_frame.header.Http_frame.Http_header.headers
-                     in
-                     let encoded =
-                       let n = String.length credentials in
-                       if n > 6 && String.sub credentials 0 6 = "Basic " then
-                         String.sub credentials 6 (n-6)
-                       else
-                         failwith "credentials"
-                     in
-                     let decoded = Netencoding.Base64.decode encoded in
-                     let i = String.index decoded ':' in
-                     (String.sub decoded 0 i,
-                      String.sub decoded (i+1) (String.length decoded - (i+1)))
-                   in
-                   auth login password >>=
-                     (fun r ->
-                        if r then begin
-                          Messages.debug2 "--Access control (auth): valid credentials!";
-                          Lwt.return (Extensions.Ext_next err)
-                        end
-                        else reject ())
-                 with
-                   | Not_found -> reject ()
-                   | e ->
-                       Messages.debug
-                         (fun () -> sprintf
-                            "--Access control (auth): Invalid Authorization header (%s)"
-                            (Printexc.to_string e));
-                       fail (Ocsigen_http_error (Http_frame.Cookies.empty, 400))
-                 end
-             | Extensions.Req_found (ri, r) ->
-                 Lwt.return (Extensions.Ext_found r))
 
     | Element ("notfound", [], []) ->
         (fun rs ->
