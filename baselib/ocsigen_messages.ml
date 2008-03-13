54a51313b64373afa75c7213691105226c623f0e
Stephane Glondu <steph@glondu.net>
2008-03-13 14:49:42 +0100

    Using YYYY-MM-DD format for logs
    darcs-hash:20080313134942-0445d-0060fcb0323d42cfee48e4e81302e4be59b61826

--- baselib/ocsigen_messages.ml
+++ baselib/ocsigen_messages.ml
@@ -60,10 +60,10 @@ let log_aux file console_print s =
   let date = 
     let t = Unix.localtime (Unix.time ()) in
     Printf.sprintf 
-      "%02d-%02d-%04d %02d:%02d:%02d" 
-      t.Unix.tm_mday 
-      (t.Unix.tm_mon + 1)
+      "%04d-%02d-%02d %02d:%02d:%02d"
       (1900 + t.Unix.tm_year)
+      (t.Unix.tm_mon + 1)
+      t.Unix.tm_mday
       t.Unix.tm_hour
       t.Unix.tm_min
       t.Unix.tm_sec 
