5f736dd5c29e77d1d0d455ef2ebb8ea818dbf9bd
boris <boris@yakobowski.org>
2009-10-13 14:32:58 +0200

    Better complexity for the jsmarshal function
    Ignore-this: 2d99c95aee26272d3ac48fa184debfa9
    
    darcs-hash:20091013123258-5c728-7008cef737616be1e8e92b959c722bb301572538

--- eliom/eliom_obrowser.ml
+++ eliom/eliom_obrowser.ml
@@ -1,10 +1,15 @@
+let string_map f s =
+  let r = ref [] in
+  for i = String.length s - 1 downto 0 do
+    r := f s.[i] :: !r
+  done;
+  !r
+
 let jsmarshal v =
   let s = Marshal.to_string v [] in
-  let rec pp i =
-    if i < 0 then ""
-    else if i = 0 then Printf.sprintf "0x%02X" (Char.code s.[i])
-    else pp (pred i) ^ "," ^ Printf.sprintf "0x%02X" (Char.code s.[i])
-  in "[" ^ pp (String.length s - 1) ^ "]"
+  let s' = string_map (fun c -> Printf.sprintf "0x%02X" (Char.code c)) s in
+  Printf.sprintf "[%s]" (String.concat "," s')
+
 
 let fresh_id = 
   let c = ref 0 in
