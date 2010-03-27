5ff3747b67b00a1b2464c3b681cdd7850c5c9743
raphael <none>
2010-03-27 13:17:05 +0100

    Introducing comet.ml (and modification to Makefile)
    Ignore-this: e6554299b57c03cd9a96bc999bcd5b01
    This is a preliminary version. Changes will happen.
    
    darcs-hash:20100327121705-ec676-b8a9a18d8282027f96f0effe74bf8ecd46cfa2f8

---
+++ extensions/comet.ml
@@ -0,0 +1,255 @@
+(* Comet server extension for ocsigen *)
+(* /!\ PROTOTYPE /!\ *)
+(* For now only an UDP style connection is available :
+ * a module can use :
+ *   Comet.Channels.new_channel to obtain a channel
+ *   Comet.Channels.get_id to communicate a channel's id to a client
+ *   Comet.Channels.write to send a message to all connected clients currently
+ *     listening on this channel
+ *   Comet.Channels.destroy_channel if a channel is not needed anymore
+ *
+ * Clients have to start a new connection from time to time (see [timeout] in
+ * order to recheck the server. The connection is then held by the server
+ * waiting for a channel to be written on. Every client listening to this
+ * channel is then sent the value written on the channel. If a client is
+ * disconnected, nothing happens !
+ *
+ * Eg :
+ *
+ * let ch = Comet.Channels.new_channel () (* as a global value for the module *)
+ *
+ * let serve_page () =
+ *   generate_script_using_ch (Comet.Channels.get_id ch) >>= fun script ->
+ *   serve_page_with_script_to_client script other_parameter
+ *
+ * let rec tick =
+ *   let count = ref 0 in
+ *   fun () ->
+ *     incr c ;
+ *     Comet.Channels.write ch (string_of_int !count) ;
+ *     Lwt_unix.sleep 10. >>= tick
+ *
+ *)
+
+module OFrame = Ocsigen_http_frame
+module OStream = Ocsigen_stream
+module OX = Ocsigen_extensions
+module Pxml = Simplexmlparser
+let ( >>= ) = Lwt.bind
+
+(* a tiny addition to Lwt library : filter_map *)
+let filter_map_rev_s f l =
+  let rec aux accu = function
+    | [] -> Lwt.return accu
+    | x::xs -> f x >>= function
+        | Some y -> aux (y :: accu) xs
+        | None -> aux accu xs
+  in aux [] l
+let filter_map_s f l =
+  filter_map_rev_s f l >>= fun l -> Lwt.return (List.rev l)
+
+(* timeout for comet connections : if no value has been written in the ellapsed
+* time, connection will be closed. Should be equal to client timeout. *)
+let timeout = 20.
+
+(* HOWTO limit the number of connections (to avoid DOS) *)
+module Channels :
+sig
+
+  type t
+    (* the type of channels :
+     * channels can be written on or read from
+     *)
+
+  val new_channel : unit -> t
+    (* channels carrying 'a values *)
+  val write  : t -> string -> unit
+    (* [write ch v] sends v over ch *)
+  val read : t -> string Lwt.t
+    (* [read ch] will return whenever a value is written on it. The result is
+     * the value written on it *)
+
+  val find_channel : int -> t
+    (* may raise Not_found if the channel was destroyed *)
+  val get_id : t -> int
+    (* [find_channel (get_id ch)] returns [ch] *)
+
+  val destroy_channel : t -> unit
+    (* Makes the subsequent calls to find_channel fail for this channel, also
+     * makes the channel GCable as far as this module is concerned. If other
+     * references are kept, it won't be. *)
+
+end = struct
+
+  module CMap = Map.Make (struct type t = int let compare = compare end)
+
+  type t =
+      {
+        ch_id : int ;
+        mutable ch_reader : string Lwt.t ;
+        mutable ch_writer : string Lwt.u ;
+      }
+
+  let cmap : t CMap.t ref = ref CMap.empty
+  let new_id = let c = ref 0 in fun () -> incr c ; !c
+
+  let find_channel i = CMap.find i !cmap
+
+  let get_id ch = ch.ch_id
+
+  let destroy_channel ch =
+    cmap := CMap.remove ch.ch_id !cmap
+
+  let new_channel () =
+    let (reader, writer) = Lwt.wait () in
+    let ch =
+      {
+        ch_id = new_id () ;
+        ch_reader = reader ;
+        ch_writer = writer ;
+      }
+    in cmap := CMap.add ch.ch_id ch !cmap ; ch
+
+  let write ch v =
+    Lwt.wakeup ch.ch_writer v ;
+    let (reader, writer) = Lwt.wait () in
+    ch.ch_writer <- writer ;
+    ch.ch_reader <- reader
+
+  let read ch = ch.ch_reader
+
+end
+
+
+module Messages :
+  (* All about messages from between clients and server *)
+sig
+
+  val decode_incomming :
+    OX.request_info -> Channels.t list Lwt.t
+    (* decode incomming message : the result is the list of channels to listen
+     * to. *)
+
+  val encode_outgoing :
+    (Channels.t * string) option list -> string OStream.t Lwt.t
+
+end = struct
+
+  let decode_string s =
+    (* Is the split cooperative enough ? *)
+    let re = Netstring_pcre.regexp ";" in
+    Lwt.return (Netstring_pcre.split re s) >>=
+    filter_map_s
+      (fun s ->
+         Lwt.catch
+           (fun () ->
+              Lwt.return (Some (Channels.find_channel (int_of_string s)))
+           )
+           (function
+              | Not_found -> Lwt.return None
+              | exc -> Lwt.fail exc
+           )
+      )
+
+  let decode_incomming { OX.ri_http_frame = { OFrame.frame_content = fc } } =
+    match fc with
+      | None -> Lwt.return []
+      | Some fc -> OStream.string_of_stream (OStream.get fc) >>= decode_string
+
+  let rec encode_outgoing_step l =
+    let rec aux accu = function (*TODO: use a fold *)
+      | [] -> print_endline ("accu : " ^ accu) ; accu
+      | None :: tl -> aux accu tl
+      | Some (chan, str) :: tl ->
+          aux
+            (  accu
+             ^ "[" ^ string_of_int (Channels.get_id chan) ^ " : " ^ str ^ "]"
+            )
+            tl
+    in aux "" l
+ 
+  let encode_outgoing l =
+    Lwt.return (OStream.of_string (encode_outgoing_step l))
+
+
+end
+
+module Main :
+  (* binding Lwt threads *)
+  (* using Lwt.choose, a client can wait for all the channels thread on which it
+   * is bound and return with the first result. *)
+sig
+
+  val treat_incoming :
+    OX.request_info -> OFrame.result Lwt.t
+    (* treat an incoming request *)
+
+end = struct
+
+  let armless_timeout t r =
+    Lwt.catch
+      (fun () -> Lwt_unix.timeout t)
+      (function
+         | Lwt_unix.Timeout -> Lwt.return r
+         | exc -> Lwt.fail exc
+      )
+
+  let rec treat_decoded chans =
+    Lwt.choose
+      (   armless_timeout timeout None
+       :: List.map
+            (fun chan ->
+               Channels.read chan >>= fun value ->
+               print_endline ("read : " ^ value) ;
+               Lwt.return (Some (chan, value))
+            )
+            chans
+      ) >>=
+    fun x -> Lwt.return [ x ] >>= (*TODO: replace Lwt.choose with a proper "select" and delete this line *)
+    Messages.encode_outgoing
+
+  let treat_incoming r =
+    print_endline "incoming" ;
+    Messages.decode_incomming r >>= treat_decoded >>= fun stream ->
+      let res = OFrame.default_result () in
+        Lwt.return
+          { res with
+                OFrame.res_stream = (stream, None) ;
+                OFrame.res_content_length = None ;
+                OFrame.res_content_type = Some "text" ;
+          }
+
+
+end
+
+let comet_regexp = Netstring_pcre.regexp ".*x-ocsigen-comet.*"
+let main = function
+  | OX.Req_found _ ->
+      Lwt.return OX.Ext_do_nothing
+  | OX.Req_not_found (_, rq) ->
+      match rq.OX.request_info.OX.ri_content_type_string with
+        | Some s ->
+            begin match Netstring_pcre.string_match comet_regexp s 0 with
+                | Some _ ->
+                    Lwt.return
+                      (OX.Ext_found
+                         (fun () -> Main.treat_incoming rq.OX.request_info)
+                      )
+                | None -> Lwt.return OX.Ext_do_nothing
+            end
+        | None -> Lwt.return OX.Ext_do_nothing
+
+
+let begin_init () = ()
+let end_init () = ()
+let parse_config _ _ _ = function
+  | Pxml.Element ("comet", [], []) -> main
+  | Pxml.Element (t, _, _) -> raise (OX.Bad_config_tag_for_extension t)
+  | _ -> raise (OX.Error_in_config_file "Unexpected data in config file")
+let site_creator (hostpattern : OX.virtual_hosts) = parse_config
+let user_site_creator (path : OX.userconf_info) = site_creator
+let () = OX.register_extension
+  ~name:"comet"
+  ~fun_site:site_creator
+  ~user_fun_site:user_site_creator
+  ()
