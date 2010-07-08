02c352db82ed26e900339a2085f948269dab304d
raphael proust <none>
2010-07-08 17:56:22 +0200

    conflict fix in eliom_event
    Ignore-this: 3bd60831e1a0b3e0719e115e2878f601
    
    darcs-hash:20100708155622-8ea92-3fa5c749448e11ef1ba8faa79c0637a2d7da372a

--- eliom/eliom_event.ml
+++ eliom/eliom_event.ml
@@ -31,9 +31,7 @@ struct
     let chan = Eliom_comet.Dlisted_channels.create ~max_size:5 e in
     let `R r = React.E.retain e (fun () -> ()) in
     let `R _ = React.E.retain e (fun () -> r () ; ignore chan) in
-    Eliommod_client.wrap
-      ~sp
-      (Eliom_comet.Channels.get_id chan)
+    Eliom_comet.Dlisted_channels.wrap ~sp chan
 
 end
 
