4869e51d99334e33de0172b5bf4d2f6c9b3cacca
gregoire.henry <gregoire.henry@pps.jussieu.fr>
2011-04-04 21:42:22 +0200

    client: Add missing signatures in Eliom_output
    Ignore-this: 8a91ebddcbfefcd0704b3c58d7fc7540
    
    darcs-hash:20110404194222-da4f2-ee545b3e31b2436babf4fbb152600edbde2e7bdb

--- src/client/eliom_output.mli
+++ src/client/eliom_output.mli
@@ -13,5 +13,20 @@ sig
 end
 
 module Xhtml : Base
+module XhtmlForms : Base
 module Redirection : Base
+module Blocks : Base
 module Blocks5 : Base
+
+module HtmlText : Base
+module CssText : Base
+module Text : Base
+module Action : Base
+module Unit : Base
+
+module String_redirection : Base
+
+module Any : Base
+module Streamlist : Base
+
+module Caml : Base
