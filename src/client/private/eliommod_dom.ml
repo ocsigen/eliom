c3414f6ef221ec033f4bc1ea5fdf013fe2c6776a
vincent.balat at univ-paris-diderot.fr <none>
2012-05-11 21:31:19 +0200

    Bugfix in eliom/client: url with quotes in CSS were not rewritten correctly
    Ignore-this: 952991099274250ea3bf9796587367af
    
    darcs-hash:20120511193119-d035b-8f12db5d9985047a7edcb736aecc0d457db83127

--- src/client/private/eliommod_dom.ml
+++ src/client/private/eliommod_dom.ml
@@ -73,8 +73,8 @@ let slow_ancessor (elt1:#Dom.node Js.t) (elt2:#Dom.node Js.t) =
     then true
     else
       match Js.Opt.to_option n##parentNode with
-	| None -> false
-	| Some p -> check_parent p
+        | None -> false
+        | Some p -> check_parent p
   in
   check_parent (elt2:>Dom.node Js.t)
 
@@ -149,10 +149,10 @@ let slow_select_request_nodes (root:Dom_html.element Js.t) =
   let rec traverse (node:Dom.node Js.t) =
     match node##nodeType with
       | Dom.ELEMENT ->
-	let node = (Js.Unsafe.coerce node:Dom_html.element Js.t) in
-	if has_request_class node
-	then ignore (node_array##push(node));
-	iter_nodeList node##childNodes traverse
+        let node = (Js.Unsafe.coerce node:Dom_html.element Js.t) in
+        if has_request_class node
+        then ignore (node_array##push(node));
+        iter_nodeList node##childNodes traverse
       | _ -> ()
   in
   traverse (root:>Dom.node Js.t);
@@ -166,21 +166,21 @@ let slow_select_nodes (root:Dom_html.element Js.t) =
   let rec traverse (node:Dom.node Js.t) =
     match node##nodeType with
       | Dom.ELEMENT ->
-	let node = (Js.Unsafe.coerce node:Dom_html.element Js.t) in
-	let call_service,process_node,closure = has_classes node in
-	begin
-	  if call_service
-	  then
-	    match Dom_html.tagged node with
-	      | Dom_html.A e -> ignore (a_array##push(e))
-	      | Dom_html.Form e -> ignore (form_array##push(e))
-	      | _ -> error "%s element tagged as eliom link" (Js.to_string (node##tagName))
-	end;
-	if process_node
-	then ignore (node_array##push(node));
-	if closure
-	then ignore (closure_array##push(node));
-	iter_nodeList node##childNodes traverse
+        let node = (Js.Unsafe.coerce node:Dom_html.element Js.t) in
+        let call_service,process_node,closure = has_classes node in
+        begin
+          if call_service
+          then
+            match Dom_html.tagged node with
+              | Dom_html.A e -> ignore (a_array##push(e))
+              | Dom_html.Form e -> ignore (form_array##push(e))
+              | _ -> error "%s element tagged as eliom link" (Js.to_string (node##tagName))
+        end;
+        if process_node
+        then ignore (node_array##push(node));
+        if closure
+        then ignore (closure_array##push(node));
+        iter_nodeList node##childNodes traverse
       | _ -> ()
   in
   traverse (root:>Dom.node Js.t);
@@ -265,32 +265,32 @@ let add_childrens (elt:Dom_html.element Js.t) (sons:Dom.node Js.t list) =
   with
     | exn ->
       (* this code is ie only, there are no reason for an appendChild
-	 to fail normally *)
+         to fail normally *)
       let concat l =
-	let rec concat acc = function
-	  | [] -> acc
-	  | t::q ->
-	    let txt =
-	      match (Dom.nodeType t) with
-		| Dom.Text t -> t
-		| _ -> error "add_childrens: not text node in tag %s" (Js.to_string (elt##tagName)) in
-	    concat (acc##concat(txt##data)) q
-	in
-	concat (Js.string "") l
+        let rec concat acc = function
+          | [] -> acc
+          | t::q ->
+            let txt =
+              match (Dom.nodeType t) with
+                | Dom.Text t -> t
+                | _ -> error "add_childrens: not text node in tag %s" (Js.to_string (elt##tagName)) in
+            concat (acc##concat(txt##data)) q
+        in
+        concat (Js.string "") l
       in
       match Dom_html.tagged elt with
-	| Dom_html.Script elt ->
-	  elt##text <- concat sons
-	| Dom_html.Style elt ->
-	  (* we need to append the style node to something. If we
-	     don't do that the styleSheet field is not created if we.
-	     And we can't do it by creating it with the ie specific
-	     document.createStyleSheet: the styleSheet field is not
-	     initialised and it can't be set either. *)
-	  let d = Dom_html.createHead Dom_html.document in
-	  Dom.appendChild d elt;
-	  (Js.Unsafe.coerce elt)##styleSheet##cssText <- concat sons
-	| _ -> debug_exn "add children: can't appendChild" exn; raise exn
+        | Dom_html.Script elt ->
+          elt##text <- concat sons
+        | Dom_html.Style elt ->
+          (* we need to append the style node to something. If we
+             don't do that the styleSheet field is not created if we.
+             And we can't do it by creating it with the ie specific
+             document.createStyleSheet: the styleSheet field is not
+             initialised and it can't be set either. *)
+          let d = Dom_html.createHead Dom_html.document in
+          Dom.appendChild d elt;
+          (Js.Unsafe.coerce elt)##styleSheet##cssText <- concat sons
+        | _ -> debug_exn "add children: can't appendChild" exn; raise exn
 
 (* END IE HACK *)
 
@@ -302,11 +302,11 @@ let copy_element (e:Dom.element Js.t)
       (e##getAttribute(Js.string Eliom_pervasives_base.RawXML.node_id_attrib)) in
     match node_id with
       | Some id when registered_process_node id ->
-	Js.Opt.iter
-	  (e##getAttribute(Js.string "class"))
-	  (fun classes -> copy##setAttribute(Js.string "class",classes));
-	copy##setAttribute(Js.string Eliom_pervasives_base.RawXML.node_id_attrib,id);
-	Some copy
+        Js.Opt.iter
+          (e##getAttribute(Js.string "class"))
+          (fun classes -> copy##setAttribute(Js.string "class",classes));
+        copy##setAttribute(Js.string Eliom_pervasives_base.RawXML.node_id_attrib,id);
+        Some copy
       | _ ->
         let add_attribute a =
           Js.Opt.iter (Dom.CoerceTo.attr a)
@@ -336,13 +336,13 @@ let html_document (src:Dom.element Dom.document Js.t) registered_process_node: D
   match Js.Opt.to_option (Dom_html.CoerceTo.element content) with
     | Some e ->
       begin
-	try Dom_html.document##adoptNode((e:>Dom.element Js.t)) with
-	  | exn ->
-	    debug_exn "can't addopt node, import instead" exn;
-	    try Dom_html.document##importNode((e:>Dom.element Js.t),Js._true) with
-	      | exn ->
-		debug_exn "can't import node, copy instead" exn;
-		copy_element content registered_process_node
+        try Dom_html.document##adoptNode((e:>Dom.element Js.t)) with
+          | exn ->
+            debug_exn "can't addopt node, import instead" exn;
+            try Dom_html.document##importNode((e:>Dom.element Js.t),Js._true) with
+              | exn ->
+                debug_exn "can't import node, copy instead" exn;
+                copy_element content registered_process_node
       end
     | None ->
       debug "can't addopt node, document not parsed as html. copy instead";
@@ -370,12 +370,12 @@ let basedir path =
     match Regexp.matched_group res 1 with
     | None ->
       ( match Regexp.matched_group res 3 with
-	| Some ".." -> "../"
-	| _ -> "/" )
+        | Some ".." -> "../"
+        | _ -> "/" )
     | Some dir ->
       ( match Regexp.matched_group res 3 with
-	| Some ".." -> dir^"../"
-	| _ -> dir )
+        | Some ".." -> dir^"../"
+        | _ -> dir )
 
 let fetch_linked_css e =
   let rec extract acc (e : Dom.node Js.t) =
@@ -383,15 +383,15 @@ let fetch_linked_css e =
     | Dom.Element e when is_stylesheet e ->
         let e : Dom_html.linkElement Js.t = Js.Unsafe.coerce e in
         let href = e##href in
-	if
+        if
           Js.to_bool e##disabled || e##title##length > 0 || href##length = 0
         then
-	  acc
-	else
+          acc
+        else
           let href = Js.to_string href in
-	  let css =
-	    Eliom_request.http_get href [] Eliom_request.string_result in
-	  acc @ [e, (e##media, href, css >|= snd)]
+          let css =
+            Eliom_request.http_get href [] Eliom_request.string_result in
+          acc @ [e, (e##media, href, css >|= snd)]
     | Dom.Element e ->
         let c = e##childNodes in
         let acc = ref acc in
@@ -407,43 +407,48 @@ let dbl_quoted_url_raw = "\"(([^\\\\\"]|\\\\.)*)\""
 let quoted_url_raw     =  "'(([^\\\\']|\\\\.)*)'"
 let url_re =
   Regexp.regexp (Printf.sprintf "url\\s*\\(\\s*(%s|%s|%s)\\s*\\)\\s*"
-		   dbl_quoted_url_raw
-		   quoted_url_raw
-		   url_content_raw)
+                   dbl_quoted_url_raw
+                   quoted_url_raw
+                   url_content_raw)
 let raw_url_re =
   Regexp.regexp (Printf.sprintf "\\s*(%s|%s)\\s*"
-		   dbl_quoted_url_raw
-		   quoted_url_raw)
+                   dbl_quoted_url_raw
+                   quoted_url_raw)
 
 let absolute_re = Regexp.regexp "\\s*(https?:\\/\\/|\\/)"
 let absolute_re2 = Regexp.regexp "['\\\"]\\s*((https?:\\/\\/|\\/).*)['\\\"]$"
 
 exception Incorrect_url
+
 let parse_absolute ~prefix href =
   match Regexp.search absolute_re href 0 with
   | Some (i, _) when i=0 -> (* absolute URL -> do not rewrite *) href
   | _ -> 
     match Regexp.search absolute_re2 href 0 with
       | Some (i, res) when i = 0 ->
-	(match Regexp.matched_group res 1 with
-	  | Some href -> (* absolute URL -> do not rewrite *) href
-	  | None -> raise Incorrect_url)
+        (match Regexp.matched_group res 1 with
+          | Some href -> (* absolute URL -> do not rewrite *) href
+          | None -> raise Incorrect_url)
       | _ -> prefix ^ href
 
 let parse_url ~prefix css pos =
   match Regexp.search url_re css pos with
   | Some (i, res) when i = pos ->
       ( i + String.length (Regexp.matched_string res),
-	match Regexp.matched_group res 1 with
-	| Some href -> parse_absolute ~prefix href
-	| None -> raise Incorrect_url )
+        match Regexp.matched_group res 2 with
+        | Some href -> parse_absolute ~prefix href
+        | None -> (match Regexp.matched_group res 3 with
+            | Some href -> parse_absolute ~prefix href
+            | None -> (match Regexp.matched_group res 4 with
+                | Some href -> parse_absolute ~prefix href
+                | None -> raise Incorrect_url )))
   | _ ->
       match Regexp.search raw_url_re css pos with
       | Some (i, res) when i = pos ->
-	  ( i + String.length (Regexp.matched_string res),
-	    match Regexp.matched_group res 1 with
-	    | Some href -> parse_absolute ~prefix href
-	    | None -> raise Incorrect_url )
+          ( i + String.length (Regexp.matched_string res),
+            match Regexp.matched_group res 1 with
+            | Some href -> parse_absolute ~prefix href
+            | None -> raise Incorrect_url )
       | _ -> raise Incorrect_url
 
 let parse_media css pos =
@@ -463,17 +468,17 @@ let rewrite_css_url ~prefix css pos =
     if pos < String.length css then
       match Regexp.search url_re css pos with
       | None ->
-	  Buffer.add_substring buf css pos (String.length css - pos)
+          Buffer.add_substring buf css pos (String.length css - pos)
       | Some (i, res) ->
-	  Buffer.add_substring buf css pos (i - pos);
-	  try
-	    let i, href = parse_url ~prefix css i in
+          Buffer.add_substring buf css pos (i - pos);
+          try
+            let i, href = parse_url ~prefix css i in
             Buffer.add_string buf "url('";
             Buffer.add_string buf href;
             Buffer.add_string buf "')";
-	    rewrite i
-	  with Incorrect_url ->
-	    Buffer.add_substring buf css i (String.length css - i)
+            rewrite i
+          with Incorrect_url ->
+            Buffer.add_substring buf css i (String.length css - i)
   in
   rewrite pos;
   Buffer.contents buf
@@ -487,12 +492,12 @@ let rec rewrite_css ~max (media, href, css) =
     | Some css ->
         if !Eliom_config.debug_timings then
           Firebug.console##time(Js.string ("rewrite_CSS: "^href));
-	lwt imports, css  =
-	  rewrite_css_import ~max ~prefix:(basedir href) ~media css 0
-	in
-	if !Eliom_config.debug_timings then
+        lwt imports, css  =
+          rewrite_css_import ~max ~prefix:(basedir href) ~media css 0
+        in
+        if !Eliom_config.debug_timings then
           Firebug.console##timeEnd(Js.string ("rewrite_CSS: "^href));
-	Lwt.return (imports @ [(media,  css)])
+        Lwt.return (imports @ [(media,  css)])
   with e ->
     Lwt.return [(media, Printf.sprintf "@import url(%s);" href)]
 
@@ -506,35 +511,35 @@ and rewrite_css_import ?(charset = "") ~max ~prefix ~media css pos =
       let init = String.sub css pos (i - pos) in
       let charset = if pos = 0 then init else charset in
       try
-	let i = i + String.length (Regexp.matched_string res) in
-	let i, href = parse_url ~prefix css i in
-	let i, media' = parse_media css i in
-	lwt import =
-	  if max = 0 then
-	    (* Maximum imbrication of @import reached, rewrite url. *)
-	    Lwt.return [(media,
-			 Printf.sprintf "@import url('%s') %s;\n" href media')]
-	  else if media##length > 0 && String.length media' > 0 then
-	    (* TODO combine media if possible...
-	       in the mean time keep explicit import. *)
-	    Lwt.return [(media,
-	                 Printf.sprintf "@import url('%s') %s;\n" href media')]
-	  else
-	    let media =
-	      if media##length > 0 then media else Js.string media'
-	    in
-	    let css =
-	      Eliom_request.http_get href [] Eliom_request.string_result in
-	    rewrite_css ~max:(max-1) (media, href, css >|= snd)
-	and imports, css =
-	  rewrite_css_import ~charset ~max ~prefix ~media css i in
-	Lwt.return (import @ imports, css)
+        let i = i + String.length (Regexp.matched_string res) in
+        let i, href = parse_url ~prefix css i in
+        let i, media' = parse_media css i in
+        lwt import =
+          if max = 0 then
+            (* Maximum imbrication of @import reached, rewrite url. *)
+            Lwt.return [(media,
+                         Printf.sprintf "@import url('%s') %s;\n" href media')]
+          else if media##length > 0 && String.length media' > 0 then
+            (* TODO combine media if possible...
+               in the mean time keep explicit import. *)
+            Lwt.return [(media,
+                         Printf.sprintf "@import url('%s') %s;\n" href media')]
+          else
+            let media =
+              if media##length > 0 then media else Js.string media'
+            in
+            let css =
+              Eliom_request.http_get href [] Eliom_request.string_result in
+            rewrite_css ~max:(max-1) (media, href, css >|= snd)
+        and imports, css =
+          rewrite_css_import ~charset ~max ~prefix ~media css i in
+        Lwt.return (import @ imports, css)
       with
       | Incorrect_url ->
-	  Lwt.return ([], rewrite_css_url ~prefix css pos)
+          Lwt.return ([], rewrite_css_url ~prefix css pos)
       | e ->
-	  debug "Exc2: %s" (Printexc.to_string e);
-	  Lwt.return ([], rewrite_css_url ~prefix css pos)
+          debug "Exc2: %s" (Printexc.to_string e);
+          Lwt.return ([], rewrite_css_url ~prefix css pos)
 
 let max_preload_depth = ref 4
 
@@ -543,11 +548,11 @@ let build_style (e, css) =
   lwt css =
     Lwt_list.map_p
       (fun (media, css) ->
-	 let style = Dom_html.createStyle Dom_html.document in
-	 style##_type <- Js.string "text/css";
+         let style = Dom_html.createStyle Dom_html.document in
+         style##_type <- Js.string "text/css";
          style##media <- media;
-	 style##innerHTML <- Js.string css;
-	 Lwt.return (style :> Dom.node Js.t))
+         style##innerHTML <- Js.string css;
+         Lwt.return (style :> Dom.node Js.t))
       css in
   (* Noscript is used to group style. It's ignored by the parser when
      scripting is enabled, but does not seems to be ignore when
@@ -561,12 +566,12 @@ let preload_css (doc : Dom_html.element Js.t) =
     Firebug.console##time(Js.string "preload_css (fetch+rewrite)");
   lwt css = Lwt_list.map_p build_style (fetch_linked_css (get_head doc)) in
   List.iter (fun (e, css) ->
-	       try Dom.replaceChild (get_head doc) css e
-	       with _ ->
+               try Dom.replaceChild (get_head doc) css e
+               with _ ->
                     (* Node was a unique node that has been removed...
-		       in a perfect settings we won't have parsed it... *)
-		 Firebug.console##debug(Js.string "Unique CSS skipped...");
-		 ()) css;
+                       in a perfect settings we won't have parsed it... *)
+                 Firebug.console##debug(Js.string "Unique CSS skipped...");
+                 ()) css;
   if !Eliom_config.debug_timings then
     Firebug.console##timeEnd(Js.string "preload_css (fetch+rewrite)");
   Lwt.return ()
