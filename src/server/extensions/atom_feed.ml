69bc6d1d43ea336a7fba7d4dbcbb761da06ee618
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2012-11-30 14:59:36 +0100

    Fixing bugs in atom feed with html5 functions
    Ignore-this: d1c4a9fd340c1ab946dff1bb805862d7
    
    darcs-hash:20121130135936-a85e5-679ad82383b6d0abaf6955641b989ce957473759

--- src/server/extensions/atom_feed.ml
+++ src/server/extensions/atom_feed.ml
@@ -139,17 +139,17 @@ let html5Div content =
   let content = Eliom_content.Html5.F.toeltl content in
   let module Eliom_xml = Xml_iter.Make(Eliom_content_core.Xml) in
   let content =
-    let f ename attibs =
-      let map_attibs f =
+    let f ename attribs =
+      let map_attribs f =
         List.map
           (fun x ->
             let aname = Eliom_content_core.Xml.aname x in
             let acontent = Eliom_content_core.Xml.acontent x in
             f aname acontent
           )
-          attibs
+          attribs
       in
-      let reconstruct_attibs aname = function
+      let reconstruct_attribs aname = function
         | Eliom_content_core.Xml.AFloat x -> Xml.float_attrib aname x
         | Eliom_content_core.Xml.AInt x -> Xml.int_attrib aname x
         | Eliom_content_core.Xml.AStr x -> Xml.string_attrib aname x
@@ -157,57 +157,23 @@ let html5Div content =
             | Eliom_content_core.Xml.Space -> Xml.space_sep_attrib aname x
             | Eliom_content_core.Xml.Comma -> Xml.comma_sep_attrib aname x
       in
-      let reconstruct_url aname = function
-        | Eliom_content_core.Xml.AStr url ->
-            let url =
-              try
-                let neturl =
-                  let base_syntax =
-                    {Neturl.ip_url_syntax
-                     with Neturl.url_enable_relative = true;
-                    }
-                  in
-                  Neturl.parse_url ~base_syntax url
-                in
-                let base =
-                  let base =
-                    Eliom_request_info.get_original_full_path_string
-                      ()
-                  in
-                  let base =
-                    Eliom_lib.Url.remove_end_slash base
-                  in
-                  Neturl.parse_url (base ^ "/")
-                in
-                Neturl.string_of_url
-                  (Neturl.ensure_absolute_url ~base neturl)
-              with Neturl.Malformed_URL -> url
-            in
-            Xml.string_attrib aname url
-        | acontent -> reconstruct_attibs aname acontent
-      in
       match ename with
-        | "a" ->
-            map_attibs
+        | "a" -> (* Some atom readers doesn't like the content of these tags *)
+            map_attribs
               (fun aname acontent -> match aname with
-                | "href" -> reconstruct_url aname acontent
-                | _ -> reconstruct_attibs aname acontent
+                | "data-eliom-cookies-info"
+                | "data-eliom-node-id" -> Xml.string_attrib aname ""
+                | _ -> reconstruct_attribs aname acontent
               )
-        | "img" ->
-            map_attibs
-              (fun aname acontent -> match aname with
-                | "src" -> reconstruct_url aname acontent
-                | _ -> reconstruct_attibs aname acontent
-              )
-        | _ -> map_attibs reconstruct_attibs
+        | _ -> map_attribs reconstruct_attribs
     in
-    let leaf ename attibs =
-      let attibs = f ename attibs in
-      Xml.leaf ~a:attibs ename
+    let leaf ename attribs =
+      let attribs = f ename attribs in
+      Xml.leaf ~a:attribs ename
     in
-    let node ename attibs elts =
-      let attibs = f ename attibs in
-      Xml.node ~a:attibs ename elts
+    let node ename attribs elts =
+      let attribs = f ename attribs in
+      Xml.node ~a:attribs ename elts
     in
     List.map
       (Eliom_xml.fold
@@ -221,7 +187,10 @@ let html5Div content =
       )
       content
   in
-  Xml.node "div" content
+  Xml.node
+    ~a:[Xml.string_attrib "xmlns" "http://www.w3.org/1999/xhtml"]
+    "div"
+    content
 
 let inlineC ?(meta = []) ?(html = false) c = `Content (Xml.node ~a:(a_type (if
             html then "html" else "text") :: metaAttr_extract meta) "content"
@@ -233,7 +202,8 @@ let xhtmlC ?(meta = []) c = `Content (Xml.node ~a:(a_type "xhtml" ::
 let html5C ?(meta = []) c =
   `Content
     (Xml.node
-       ~a:(a_type "html" :: metaAttr_extract meta)
+       (* Needs xhtml (not html) *)
+       ~a:(a_type "xhtml" :: metaAttr_extract meta)
        "content"
        [html5Div c]
     )
@@ -318,7 +288,8 @@ let xhtml ?(meta = []) content = (Xml.string_attrib "type" "xhtml" ::
       metaAttr_extract meta, [xhtmlDiv content])
 
 let html5 ?(meta = []) content =
-  (Xml.string_attrib "type" "html" :: metaAttr_extract meta,
+  (* Needs xhtml (not html) *)
+  (Xml.string_attrib "type" "xhtml" :: metaAttr_extract meta,
    [html5Div content]
   )
 
