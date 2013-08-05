85343c0e9ae318e0131917c9163403eaefc7f836
Vincent Balat <vincent.balat@univ-paris-diderot.fr>
2013-08-05 16:40:13 +0200

    Making calls to server function use absolute URL (to work with mobile apps)

--- src/client/eliom_pervasives.ml
+++ src/client/eliom_pervasives.ml
@@ -12,6 +12,10 @@ let () =
   Eliom_unwrap.register_unwrapper
     (Eliom_unwrap.id_of_int Eliom_common_base.server_function_unwrap_id_int)
     (fun (service, _) ->
-      Eliom_client.call_caml_service ~service ())
+      (* 2013-07-31 I make all RPC's absolute because otherwise
+         it does not work with mobile apps.
+         Is it a problem?
+         -- Vincent *)
+      Eliom_client.call_caml_service ~absolute:true ~service ())
 
 let _force_link = ()
