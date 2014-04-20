9fb98d68fcea0bbf048e213f980aa2eb8a31052b
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-04-20 17:47:39 +0200

    Move Ocamlbuild_eliom_core to Ocamlbuild_eliom

--- src/ocamlbuild/ocamlbuild_eliom.mli
+++ src/ocamlbuild/ocamlbuild_eliom.mli
@@ -1,4 +1,12 @@
-module Make (Eliom : Ocamlbuild_eliom_core.ELIOM) : sig
+module type ELIOM = sig
+  val server_dir : string
+  val type_dir : string
+  val client_dir : string
+end
+
+module Make (Eliom : ELIOM) : sig
+  val dispatcher_without_js_of_ocaml_support : Ocamlbuild_plugin.hook -> unit
+
   val dispatcher : Ocamlbuild_plugin.hook -> unit
 
   val dispatcher_with_oasis_support :
