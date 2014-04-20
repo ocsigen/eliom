aa8089bb4e055ad24beb73e4171bfe6c4ea66c19
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2014-04-20 16:38:39 +0200

    Ocamlbuild plugin: Add the oasis support

--- src/ocamlbuild/ocamlbuild_eliom.ml
+++ src/ocamlbuild/ocamlbuild_eliom.ml
@@ -7,4 +7,10 @@ module Make (Eliom : Ocamlbuild_eliom_core.ELIOM) = struct
   let dispatcher hook =
     Ocamlbuild_js_of_ocaml.dispatcher hook;
     M.dispatcher hook
+
+  let dispatcher_with_oasis_support ~executables hook =
+    Ocamlbuild_js_of_ocaml.dispatcher_with_oasis_support
+      ~executables
+      hook;
+    M.dispatcher hook
 end
--- src/ocamlbuild/ocamlbuild_eliom.mli
+++ src/ocamlbuild/ocamlbuild_eliom.mli
@@ -1,3 +1,8 @@
 module Make (Eliom : Ocamlbuild_eliom_core.ELIOM) : sig
   val dispatcher : Ocamlbuild_plugin.hook -> unit
+
+  val dispatcher_with_oasis_support :
+    executables:string list ->
+    Ocamlbuild_plugin.hook ->
+    unit
 end
