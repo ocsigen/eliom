86166330a6d1d56f462762d5b5928a168dae6d69
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-06-10 17:11:14 +0200

    fix typos in eliom_client wrt service typing

--- src/lib/eliom_client.client.mli
+++ src/lib/eliom_client.client.mli
@@ -43,7 +43,7 @@ val change_page :
   ?absolute:bool ->
   ?absolute_path:bool ->
   ?https:bool ->
-  service:('a, 'b,[<Eliom_service.getpost  > `Get `Post],[<Eliom_service.attached_kind],[< Eliom_service.service_kind],
+  service:('a, 'b,[<Eliom_service.getpost  > `Get `Post],[<Eliom_service.attached],[< Eliom_service.service_kind],
            [< `WithSuffix | `WithoutSuffix ], 'd, 'e,
            [< Eliom_service.registrable ], [< Eliom_registration.non_ocaml_service ])
           Eliom_service.service ->
@@ -68,7 +68,7 @@ val call_ocaml_service :
   ?absolute_path:bool ->
   ?https:bool ->
   service:('a, 'b,
-           [<Eliom_service.getpost > `Get `Post],[<Eliom_service.attached_kind ],[< Eliom_service.service_kind],
+           [<Eliom_service.getpost > `Get `Post],[<Eliom_service.attached ],[< Eliom_service.service_kind],
            [< `WithSuffix | `WithoutSuffix ], 'd, 'e,
            [< Eliom_service.registrable ], 'return Eliom_service.ocaml_service)
     Eliom_service.service ->
@@ -88,7 +88,7 @@ val exit_to :
   ?absolute_path:bool ->
   ?https:bool ->
   service:('a, 'b,
-           [<Eliom_service.getpost > `Get `Post ],[<Eliom_service.attached_kind],[< Eliom_service.service_kind],
+           [<Eliom_service.getpost > `Get `Post ],[<Eliom_service.attached],[< Eliom_service.service_kind],
            [< `WithSuffix | `WithoutSuffix ], 'd, 'e,
            [< Eliom_service.registrable ], [< Eliom_registration.non_ocaml_service ])
           Eliom_service.service ->
@@ -108,7 +108,7 @@ val window_open :
   ?https:bool ->
   service:('a, unit,
            [< Eliom_service.get_service_kind ],
-           [< Eliom_service.attached_kind],[< Eliom_service.service_kind],
+           [< Eliom_service.attached],[< Eliom_service.service_kind],
            [< `WithSuffix | `WithoutSuffix ], _, unit,
            [< Eliom_service.registrable ], _)
           Eliom_service.service ->
