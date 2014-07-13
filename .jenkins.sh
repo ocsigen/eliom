1616a76b07f41089010d44425e3e8ecba9864fc9
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-13 03:05:26 +0200

    Update .jenkins.sh (bis)

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,5 +1,5 @@
 opam pin add --no-action eliom .
-opam pin add --no-action ocsigenserver https://github.com/ocsigen/ocsigenserver.git
+opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam install --deps-only eliom
 opam install --verbose eliom
 opam remove --verbose eliom
