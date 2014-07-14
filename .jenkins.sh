a10d0bc645110f13d4d3a6c682630aed6ecde5b7
Grégoire Henry <gregoire.henry@ocamlpro.com>
2014-07-14 18:10:37 +0200

    Jenkins: build_doc.

--- .jenkins.sh
+++ .jenkins.sh
@@ -2,4 +2,16 @@ opam pin add --no-action eliom .
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam install --deps-only eliom
 opam install --verbose eliom
-opam remove --verbose eliom
+
+do_build_doc () {
+  make wikidoc
+  cp -Rf doc/manual-wiki/*.wiki $(MANUAL_SRC_DIR)
+  mkdir -p $(API_DIR)/server $(API_DIR)/client
+  cp -Rf _build/src/lib/server/api.wikidocdir/*.wiki $(API_DIR)/server/
+  cp -Rf _build/src/lib/client/api.wikidocdir/*.wiki $(API_DIR)/client/
+  cp -Rf doc/index.wiki $(API_DIR)/
+}
+
+do_remove () {
+  opam remove --verbose elimo
+}
