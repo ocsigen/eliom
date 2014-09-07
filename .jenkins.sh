3b90d18253e09b25716df83ef674a3ba7db48038
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-09-07 13:15:46 +0200

    jenkins

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,4 +1,5 @@
 opam pin add --no-action eliom .
+opam pin add --no-action deriving 'https://github.com/ocsigen/deriving.git#master'
 opam pin add --no-action tyxml 'https://github.com/ocsigen/tyxml.git#master'
 opam pin add --no-action ocsigenserver 'https://github.com/ocsigen/ocsigenserver.git#master'
 opam pin add --no-action js_of_ocaml 'https://github.com/ocsigen/js_of_ocaml.git#master'
