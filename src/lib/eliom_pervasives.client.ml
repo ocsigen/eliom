cd95e0e6682460c181a97b9252a3326de4234377
Vincent Balat <vincent.balat@vblt.org>
2016-02-10 11:48:00 +0100

    Make it possible to define named server functions client-side EXPERIMENTAL

--- src/lib/eliom_pervasives.client.ml
+++ src/lib/eliom_pervasives.client.ml
@@ -7,6 +7,21 @@ type 'a fragment = 'a client_value
 
 type ('a, +'b) server_function = 'a -> 'b Lwt.t
 
+
+let server_function
+    ?scope ?options ?charset ?code ?content_type ?headers ?secure_session ~name
+    ?csrf_safe ?csrf_scope ?csrf_secure ?max_use ?timeout ?https ?error_handler
+    argument_type () =
+  let service =
+    Eliom_service.Ocaml.post_coservice'
+      ~name
+      ?csrf_safe ?csrf_scope ?csrf_secure ?max_use ?timeout ?https
+      ~post_params:Eliom_parameter.(ocaml "argument" argument_type)
+      ()
+  in
+  fun a -> Eliom_client.call_ocaml_service ~absolute:true ~service () a
+
+
 let () =
   Eliom_unwrap.register_unwrapper
     (Eliom_unwrap.id_of_int Eliom_common_base.server_function_unwrap_id_int)
