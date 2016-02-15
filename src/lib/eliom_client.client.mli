9126c1c4ed4bb8a9556cfae9a790c70b80c300cb
Vincent Balat <vincent.balat@vblt.org>
2016-02-15 13:48:47 +0100

    Adding function Eliom_client.is_client_app

--- src/lib/eliom_client.client.mli
+++ src/lib/eliom_client.client.mli
@@ -34,6 +34,16 @@ val init_client_app :
   ?port:int ->
   full_path:Eliom_lib.Url.path -> unit -> unit
 
+(** Returns whether the application is sent by a server or started on
+    client side. If called on server side, always returns [false].
+    Otherwise, it tests the presence of JS variables added automatically by
+    Eliom when the page is sent by a server.
+    Example:
+    {[ if not (Eliom_client.is_client_app ())
+ then Eliom_client.init_client_app ... ]}
+*)
+val is_client_app : unit -> bool
+
 
 (** Call a server side service and change the current page.
     If the service belongs to the same application,
