c0e8ae4a27817248f5cb4d3fc379a73c53790e6e
Vasilis Papavasileiou <git@vasilis.airpost.net>
2016-05-30 21:17:34 +0000

    Improve Eliom_content.Html.C.node documentation

--- src/lib/eliom_content.server.mli
+++ src/lib/eliom_content.server.mli
@@ -235,11 +235,21 @@ module Svg : sig
 
   end
 
-  (** Creation of content from client-side values.
-      This makes possible to insert in server side generated pages some
-      nodes that will be computed on client side (for example reactive nodes).
-  *)
+  (** Creation of content from client-side values.  This makes
+      possible to insert in server side generated pages some nodes
+      that will be computed on client side (for example reactive
+      nodes). *)
   module C : sig
+    (** [node e] is a server-side node corresponding to the
+        client-side node [e] . [node e] can be used like any other
+        server-side node.
+
+        The implementation uses an initial placeholder node that is
+        later replaced by the client node. By default, the placeholder
+        node is [span]. The [~init] argument can be used to provide a
+        custom placeholder node (e.g., one with the same tag as the
+        client node). This can be useful in contexts where [span] is
+        not allowed. *)
     val node : ?init:'a elt -> 'a elt Eliom_client_value.t -> 'a elt
     val attr : ?init:'a attrib -> 'a attrib Eliom_client_value.t -> 'a attrib
   end
