498c7159ffd4e70799f3dfd8690f504de0cdb481
Vasilis Papavasileiou <git@vasilis.airpost.net>
2016-07-27 13:39:01 +0000

    Improve Eliom_service API documentation

--- src/lib/eliom_service_sigs.shared.mli
+++ src/lib/eliom_service_sigs.shared.mli
@@ -84,6 +84,8 @@ module type S = sig
 
   include TYPES
 
+  (** {2 Definitions of services} *)
+
   type att
   type non_att
 
@@ -103,16 +105,32 @@ module type S = sig
         of parameters it uses: suffix or not.
       - ['gn] is the type of GET parameters names. See
         {!Eliom_parameter.param_name} and form generation functions
-        (e. g. {!Eliom_content.Html5.D.get_form}).
+        (e. g. {!Eliom_content.Html.D.get_form} ).
       - ['pn] is the type of POST parameters names. See
         {!Eliom_parameter.param_name} and form generation functions
-        (e. g. {!Eliom_content.Html5.D.post_form}).
+        (e. g. {!Eliom_content.Html.D.post_form} ).
       - [ 'ret] is an information on what the service returns.  See
         {!Eliom_registration.kind}. *)
   type ('get, 'post, 'meth, 'attached, 'co, 'ext, 'reg,
         +'tipo, 'gn, 'pn, +'ret) t
     constraint 'tipo = [< `WithSuffix | `WithoutSuffix ]
 
+  (** Service identifier.
+
+      In the simplest case, a service can be identified via its path
+      ([Path path]).
+
+      For the constructors [Fallback] and [Global], the service
+      doesn't have its own URL. It either shares the URL of another
+      [service] ([Fallback service]), or it can be called on any URL
+      ([Global]). Both for [Fallback service] and for [Global], we
+      internally use an automatically-generated parameter for
+      distinguishing the service from the other services sharing the
+      URL.
+
+      [External server path] allows defining services handled by
+      external servers. These servers do not have to run
+      Ocsigen/Eliom. *)
   type (_, _, _, _, _, _, _) id =
     | Path :
         Eliom_lib.Url.path
@@ -127,40 +145,71 @@ module type S = sig
         string * Eliom_lib.Url.path
       -> (att, non_co, ext, non_reg, _, _, _) id
 
-  (** {2 Definitions of services}
+  (** Service definition.
 
-      {e Warning: These functions must be called when the site
-      information is available, that is, either during a request or
-      during the initialisation phase of the site.  Otherwise, it will
-      raise the exception
-      {!Eliom_common.Eliom_site_information_not_available}.  If you
-      are using static linking, you must delay the call to this
-      function until the configuration file is read, using
-      {!Eliom_service.register_eliom_module}. Otherwise you will also
-      get this exception.}  *)
+      The function [create ~id ~meth ()] creates a {!service}
+      identified as per [id] (see {!id} ) and accepting parameters as
+      per [meth] (see {!Eliom_service_sigs.TYPES.meth} ).
 
-  (** The function [create ~id ~meth ~ret ()] creates a {!service}
-      identified as per [id] and accepting parameters as per [m]. The
-      parameter [~ret] is used to constrain the type parameter ['ret] of
-      the service.
+      In addition to [~id] and [~meth], [create] accepts a series of
+      optional arguments described below.
 
-      If the optional parameter [~https:true] is given, all links
-      towards that service will use https. By default, links will keep
-      the current protocol.
+      If [~https:true] is provided, all links towards that service
+      will use HTTPS. By default, links will keep the current
+      protocol.
 
-      The optional parameter [~priority] allows one to change the
-      priority order between service that shares the same path. The
-      default priority is 0 ; if you want the service to be tried
+      The optional argument [~priority] allows one to change the
+      priority order between services that share the same path. The
+      default priority is 0. If you want the service to be tried
       before (resp. after) other services, put a higher (resp. lower)
       priority.
 
-      If the optional parameter [~keep_nl_params:`Persistent]
+      The remaining arguments are ignored for services identified by a
+      path (constructor [Path]). We describe their meaning in
+      conjunction with [Fallback] and [Global].
+
+      The optional [~timeout] argument specifies a timeout (in
+      seconds) after which the coservice will disappear. This amount
+      of time is computed from the creation or from the last call to
+      the service. The default is "no timeout". The optional
+      [~max_use] argument specifies that the service can be used only
+      a fixed number of times. The default is "no limitation".
+
+      If the optional argument [~keep_nl_params:`Persistent]
       (resp. [~keep_nl_params:`All]) is given, all links towards that
       service will keep persistent (resp. all) non localized GET
-      parameters of the current service. The default is [`None]. See
+      arguments of the current service. The default is [`None]. See
       the eliom manual for more information about {% <<a_manual
       chapter="params" fragment="nonlocalizedparameters"|non localized
-      parameters>>%}.  *)
+      ptimarameters>>%}.
+
+      The optional [~name] argument provides a name for the service;
+      otherwise, it will be anonymous (with an auto-generated internal
+      name).
+
+      If the optional [~csrf_safe] argument is [true], it will create
+      a {% <<a_manual chapter="security" fragment="csrf"|"CSRF-safe"
+      service>>%}. In that case the [~name] argument is ignored. The
+      default is [false].
+
+      The [~csrf_scope] and [~csrf_secure], if present, should
+      respectively correspond to the [~scope] and [~secure] arguments
+      that will be given to the associated [register]
+      function. Otherwise the registration will fail with
+      {Eliom_service.Wrong_session_table_for_CSRF_safe_coservice}. See
+      {!Eliom_registration.Html.register},
+      {!Eliom_registration.App.register} or any other
+      {!Eliom_registration}[.*.register] functions for a description
+      of these arguments.
+
+      {e Warning: [create] must be called when the site information is
+      available, that is, either during a request or during the
+      initialisation phase of the site.  Otherwise, it will raise the
+      exception {!Eliom_common.Eliom_site_information_not_available}.
+      If you are using static linking, you must delay the call to this
+      function until the configuration file is read, using
+      {!Eliom_service.register_eliom_module}. Otherwise you will also
+      get this exception.}  *)
   val create :
     ?name:string ->
     ?csrf_safe: bool ->
