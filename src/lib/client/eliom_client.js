184ca62344e0db4b3127aa7355f9b7f8cc4a28c0
Julien Sagot <1826552+sagotch@users.noreply.github.com>
2018-04-18 11:56:50 +0200

    caml_bytes_of_string -> caml_jsbytes_of_string. (#557)

--- src/lib/client/eliom_client.js
+++ src/lib/client/eliom_client.js
@@ -3,9 +3,9 @@
 //Provides: caml_unwrap_value_from_string mutable
 //Requires: caml_failwith, caml_marshal_constants
 //Requires: caml_int64_float_of_bits, caml_int64_of_bytes, caml_new_string
-//Requires: caml_bytes_of_string
+//Requires: caml_jsbytes_of_string
 var caml_unwrap_value_from_string = function (){
-  function StringReader (s, i) { this.s = caml_bytes_of_string(s); this.i = i; }
+  function StringReader (s, i) { this.s = caml_jsbytes_of_string(s); this.i = i; }
   StringReader.prototype = {
     read8u:function () { return this.s.charCodeAt(this.i++); },
     read8s:function () { return this.s.charCodeAt(this.i++) << 24 >> 24; },
