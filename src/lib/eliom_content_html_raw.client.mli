f3db2310b85bd1ed325664b31ce27a8d481b7372
Jérôme.Vouillon <jvouillon@besport.com>
2019-10-25 17:26:14 +0200

    Name event handlers

--- src/lib/eliom_content_html_raw.client.mli
+++ src/lib/eliom_content_html_raw.client.mli
@@ -2,16 +2,21 @@ open Js_of_ocaml
 type +'a elt
 type +'a attrib
 
+type event_handler = Dom_html.event Js.t -> unit
+type mouse_event_handler = Dom_html.mouseEvent Js.t -> unit
+type keyboard_event_handler = Dom_html.keyboardEvent Js.t -> unit
+type touch_event_handler = Dom_html.touchEvent Js.t -> unit
+
 module type RAW =
   Html_sigs.T
   with type 'a Xml.W.t = 'a
    and type 'a Xml.W.tlist = 'a list
    and type ('a,'b) Xml.W.ft = 'a -> 'b
    and type Xml.uri = string
-   and type Xml.event_handler = Dom_html.event Js.t -> unit
-   and type Xml.mouse_event_handler = Dom_html.mouseEvent Js.t -> unit
-   and type Xml.keyboard_event_handler = Dom_html.keyboardEvent Js.t -> unit
-   and type Xml.touch_event_handler = Dom_html.touchEvent Js.t -> unit
+   and type Xml.event_handler = event_handler
+   and type Xml.mouse_event_handler = mouse_event_handler
+   and type Xml.keyboard_event_handler = keyboard_event_handler
+   and type Xml.touch_event_handler = touch_event_handler
    and type Xml.attrib = Eliom_content_xml.Xml.attrib
    and type Xml.elt = Eliom_content_xml.Xml.elt
    and type 'a elt = 'a elt
@@ -28,10 +33,10 @@ module R :
    and type 'a Xml.W.tlist = 'a ReactiveData.RList.t
    and type ('a,'b) Xml.W.ft = 'a -> 'b
    and type Xml.uri = string
-   and type Xml.event_handler = Dom_html.event Js.t -> unit
-   and type Xml.mouse_event_handler = Dom_html.mouseEvent Js.t -> unit
-   and type Xml.keyboard_event_handler = Dom_html.keyboardEvent Js.t -> unit
-   and type Xml.touch_event_handler = Dom_html.touchEvent Js.t -> unit
+   and type Xml.event_handler = event_handler
+   and type Xml.mouse_event_handler = mouse_event_handler
+   and type Xml.keyboard_event_handler = keyboard_event_handler
+   and type Xml.touch_event_handler = touch_event_handler
    and type Xml.attrib = Eliom_content_xml.Xml.attrib
    and type Xml.elt = Eliom_content_xml.Xml.elt
    and type 'a elt = 'a elt
--- src/lib/eliom_content_html_raw.server.ml
+++ src/lib/eliom_content_html_raw.server.ml
@@ -3,6 +3,11 @@ open Js_of_ocaml
 type +'a elt = Eliom_content_xml.Xml.elt
 type +'a attrib = Eliom_content_xml.Xml.attrib
 
+type event_handler = Dom_html.event Js.t -> unit
+type mouse_event_handler = Dom_html.mouseEvent Js.t -> unit
+type keyboard_event_handler = Dom_html.keyboardEvent Js.t -> unit
+type touch_event_handler = Dom_html.touchEvent Js.t -> unit
+
 module type RAW =
   Html_sigs.T
   with type 'a Xml.W.t = 'a
@@ -10,13 +15,13 @@ module type RAW =
    and type ('a,'b) Xml.W.ft = 'a -> 'b
    and type Xml.uri = Eliom_content_xml.Xml.uri
    and type Xml.event_handler =
-         (Dom_html.event Js.t -> unit) Eliom_client_value.t
+         event_handler Eliom_client_value.t
    and type Xml.mouse_event_handler =
-         (Dom_html.mouseEvent Js.t -> unit) Eliom_client_value.t
+         mouse_event_handler Eliom_client_value.t
    and type Xml.keyboard_event_handler =
-         (Dom_html.keyboardEvent Js.t -> unit) Eliom_client_value.t
+         keyboard_event_handler Eliom_client_value.t
    and type Xml.touch_event_handler =
-         (Dom_html.touchEvent Js.t -> unit) Eliom_client_value.t
+         touch_event_handler Eliom_client_value.t
    and type Xml.elt = Eliom_content_xml.Xml.elt
    and type Xml.attrib = Eliom_content_xml.Xml.attrib
    and type 'a elt = 'a elt
