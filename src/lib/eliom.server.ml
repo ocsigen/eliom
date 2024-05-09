e7fe20591831d73912133bfc2cc7d32b10fd7d8b
Vincent Balat <vincent.balat@vblt.org>
2024-05-09 18:19:29 +0200

    Static linking: add optional parmaters for Eliom.run

--- src/lib/eliom.server.ml
+++ src/lib/eliom.server.ml
@@ -1,2 +1,20 @@
-let run ?site () =
-  Ocsigen_server.Site.register ?site (Eliom_registration.instruction ())
+let run ?site ?xhr_links ?data_timeout ?service_timeout ?persistent_timeout
+    ?max_service_sessions_per_group ?max_volatile_data_sessions_per_group
+    ?max_persistent_data_sessions_per_group ?max_service_tab_sessions_per_group
+    ?max_volatile_data_tab_sessions_per_group
+    ?max_persistent_data_tab_sessions_per_group
+    ?max_anonymous_services_per_session ?secure_cookies ?application_script
+    ?global_data_caching ?html_content_type ?ignored_get_params
+    ?ignored_post_params ?omitpersistentstorage ?eliommodule_names ()
+  =
+  Ocsigen_server.Site.register ?site
+    (Eliom_registration.instruction ?xhr_links ?data_timeout ?service_timeout
+       ?persistent_timeout ?max_service_sessions_per_group
+       ?max_volatile_data_sessions_per_group
+       ?max_persistent_data_sessions_per_group
+       ?max_service_tab_sessions_per_group
+       ?max_volatile_data_tab_sessions_per_group
+       ?max_persistent_data_tab_sessions_per_group
+       ?max_anonymous_services_per_session ?secure_cookies ?application_script
+       ?global_data_caching ?html_content_type ?ignored_get_params
+       ?ignored_post_params ?omitpersistentstorage ?eliommodule_names ())
---
+++ src/lib/eliom.server.mli
@@ -0,0 +1,36 @@
+val run :
+   ?site:Ocsigen_server.Site.t
+  -> ?xhr_links:bool
+  -> ?data_timeout:
+       [< Eliom_common.cookie_level]
+       * Eliom_common_base.scope_hierarchy option
+       * float option
+  -> ?service_timeout:
+       [< Eliom_common.cookie_level]
+       * Eliom_common_base.scope_hierarchy option
+       * float option
+  -> ?persistent_timeout:
+       [< Eliom_common.cookie_level]
+       * Eliom_common_base.scope_hierarchy option
+       * float option
+  -> ?max_service_sessions_per_group:int * bool
+  -> ?max_volatile_data_sessions_per_group:int * bool
+  -> ?max_persistent_data_sessions_per_group:int
+  -> ?max_service_tab_sessions_per_group:int * bool
+  -> ?max_volatile_data_tab_sessions_per_group:int * bool
+  -> ?max_persistent_data_tab_sessions_per_group:int
+  -> ?max_anonymous_services_per_session:int * bool
+  -> ?secure_cookies:bool
+  -> ?application_script:bool * bool
+  -> ?global_data_caching:(string list * int) option
+  -> ?html_content_type:string
+  -> ?ignored_get_params:string * Re.re
+  -> ?ignored_post_params:string * Re.re
+  -> ?omitpersistentstorage:Eliom_common.omitpersistentstorage_rule list option
+  -> ?eliommodule_names:string list
+  -> unit
+  -> unit
+(** [run ?site ()] run Eliom applications under site [site].
+    Use this to build a static executable without configuration file.
+    Default value for [?site] is default site (root of the Web site on all virtual hosts)
+    Other optional values correspond to Eliom configuration for this site. *)
