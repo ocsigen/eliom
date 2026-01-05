c0a477cdfa88d091bf6ac6c2bb531a2d67658e41
Vincent Balat <vincent.balat@vblt.org>
2026-01-05 15:50:51 +0100

    Fix compilation with OCaml 5.3

--- src/ppx/ppx_eliom_utils.ml
+++ src/ppx/ppx_eliom_utils.ml
@@ -399,7 +399,9 @@ module Cmo = struct
           Typ.class_ ~loc
             (mkloc (ident_of_out_ident id) loc)
             (List.map type_of_out_type tyl)
-      | ((Otyp_alias {aliased; alias; _}) [@if ocaml_version >= (5, 1, 0)]) ->
+      | ((Otyp_alias {aliased; alias; _}) [@if ocaml_version >= (5, 3, 0)]) ->
+          Typ.alias ~loc (type_of_out_type aliased) (mkloc (var alias) loc)
+      | ((Otyp_alias {aliased; alias; _}) [@if (ocaml_version >= (5, 1, 0)) && (ocaml_version < (5, 3, 0))]) ->
           Typ.alias ~loc (type_of_out_type aliased) (var alias)
       | ((Otyp_alias (ty, s)) [@if ocaml_version < (5, 1, 0)]) ->
           Typ.alias ~loc (type_of_out_type ty) (var s)
